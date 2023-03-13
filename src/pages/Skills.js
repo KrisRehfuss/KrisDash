f0df9a3c78de5f940bda5c05130d95f9812124bb
KrisRehfuss@gmail.com <KrisRehfuss@gmail.com>
2023-03-12 22:48:56 -0400

    hover

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,14 +2,11 @@ import Image from "next/image";
 
 function Skills({ name, color, logo }) {
   return (
-    <div className="flex rounded-lg Shadow bg-DarkG items-center w-full mb-2 p-2">
+    <div className="SkillTree">
       <div className={`rounded-xl Shadow FlexCenter p-2 ${color}`}>
-        <Image 
-         src={logo} 
-         className="w-6" 
-         alt="#" />
+        <Image src={logo} className="w-6" alt="#" />
       </div>
-      <div className="font-light ml-2 text-gray-400 ">{name}</div>
+      <div className=" ml-2   ">{name}</div>
     </div>
   );
 }
