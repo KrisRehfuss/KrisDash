38f828bb8bfe7b163ff0cf640ccb061e2447af3d
KrisRehfuss@gmail.com <KrisRehfuss@gmail.com>
2023-03-14 22:56:11 -0400

    fixed apos

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,7 +26,10 @@
             <Head>
               <title>Kris Dashboard </title>
               <meta name="description" content="Kris Dash" />
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
               <link rel="icon" href="/engine.png" />
             </Head>
 
@@ -42,19 +45,20 @@
 
                   {/* Caption */}
                   <p className=" my-5 mb-5 leading-7 text-base text-gray-400 font-semibold text-center">
-                    Hello! My name is Kris and I'm a passionate tech enthusiast aiming to apply
-                    my creativity to the world of software.
+                    Hello! My name is Kris and I&apos;m a passionate tech
+                    enthusiast aiming to apply my creativity to the world of
+                    software.
                   </p>
 
-              {/* Section */}
+                  {/* Section */}
                   <div className=" BoxFull mx-4 p-4 mt-6 mb-24">
-                {/* Live Sites */}
+                    {/* Live Sites */}
                     <div className="Section">
                       <h1 className="CaptionBlock text-left text-white">
                         Live Sites
                       </h1>
 
-                    {/* Prompt Engine */}
+                      {/* Prompt Engine */}
                       <div className="LiveSites ">
                         <div className="Round Shadow p-2 mr-4 bg-OpenAI">
                           <Image
@@ -71,7 +75,8 @@
                         >
                           Prompt <span className="text-violet-500">Engine</span>
                           <p className="text-sm text-gray-500 ">
-                            Created a prompt engine with a dynamic layout using ChatGPT's API and Tailwind
+                            Created a prompt engine with a dynamic layout using
+                            ChatGPT&apos;s API and Tailwind
                           </p>
                         </a>
                       </div>
@@ -93,28 +98,33 @@
                         >
                           <span className="text-Aero">Weather</span> App
                           <p className="text-sm text-gray-500 ">
-                            Created a miniamlist weather app 
+                            Created a miniamlist weather app
                           </p>
                         </a>
                       </div>
-
                     </div>
-
-                    
 
                     <div className="Section ">
                       <h1 className="CaptionBlock text-white">Skill Tree</h1>
                       <div className="BoxFull pt-4 lg:grid grid-cols-2 gap-x-2">
-                        <Skills logo={Next} name="Next.js" color="bg-gray-400" />
+                        <Skills
+                          logo={Next}
+                          name="Next.js"
+                          color="bg-gray-400"
+                        />
                         <Skills logo={React} name="React.js" color="bg-React" />
-                        <Skills logo={Tailwind} name="Tailwind" color="bg-gray-800" />
+                        <Skills
+                          logo={Tailwind}
+                          name="Tailwind"
+                          color="bg-gray-800"
+                        />
                         <Skills logo={Git} name="Git" color="bg-Git" />
                         <Skills logo={TS} name="TypeScript" color="bg-TS" />
                         <Skills logo={OpenAI} name="OpenAI" color="bg-OpenAI" />
                       </div>
                     </div>
 
-                  {/* Current Skill Tree */}
+                    {/* Current Skill Tree */}
                     <div className="Section mt-4">
                       {/* <h1 className="CaptionBlock text-Redd"> Recent Compositions</h1> */}
                       {/* <Video /> */}
