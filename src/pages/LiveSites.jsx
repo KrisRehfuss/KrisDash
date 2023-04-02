adeef6a996130566b4ac5bdd5b3178a6c330e2d4
KrisRehfuss@gmail.com <KrisRehfuss@gmail.com>
2023-04-01 22:24:49 -0400

    nice

--- src/pages/LiveSites.jsx
+++ src/pages/LiveSites.jsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState, useEffect } from "react";
+import Link from 'next/link'
+
 import Image from "next/image";
 import sun from '../../public/sun.png';
 import OpenAI from "../../public/openai.png";
 import { BsFillArrowDownSquareFill } from "react-icons/bs";
 import { BsSun } from "react-icons/bs";
 import Anime from '../../public/AVI.jpg'
+import Synth from '../../public/SynthB.png'
 
 
 
@@ -73,7 +76,7 @@ function LiveSites() {
          {/* Site Container */}
          <div className="flex flex-col items-center  Smoother">
 
-            {/* Weather App */}
+            {/* Weather App
             <div className="Sites ">
                <div className="Round Shadow p-2 lg:mr-4 mr-2 dark:bg-coal">
                   <Image src={sun} className="lg:w-8 w-6" alt="#" />
@@ -85,9 +88,23 @@ function LiveSites() {
                      Created a miniamlist weather app
                   </p>
                </a>
+            </div> */}
+
+            {/* Modern Synth */}
+            <div className="Sites ">
+               <div className="Round Shadow p-2 lg:mr-4 mr-2 dark:bg-gradient-to-t dark:from-Ind  to-Pinkk  dark:bg-Ind">
+                  <Image src={Synth} className="lg:w-8 w-6" alt="#" />
+               </div>
+               <Link className="w-full dark:text-gray-700 text-gray-200" href='/ModernSynth'>
+                  <h3 className="font-normal"> <span className="PrismaticText"> Modern Synth </span>
+                  </h3>
+                  <p className="lg:text-sm text-xs text-gray-400 dark:text-gray-700 ">
+                     Created a series of synths using React & Tailwind
+                  </p>
+               </Link>
             </div>
 
-            {/* Prompt Engine */}
+            {/* Prompt Engine
             <div className="Sites ">
                <div className="Round p-2 lg:mr-4 mr-2 bg-OpenAI">
                   <Image src={OpenAI} className="lg:w-8 w-6" alt="#" />
@@ -98,7 +115,7 @@ function LiveSites() {
                      Created a prompt engine with ChatGPT&apos;s API and Tailwind
                   </p>
                </a>
-            </div>
+            </div> */}
 
             {/* Bio Card */}
             <div className="Sites ">
